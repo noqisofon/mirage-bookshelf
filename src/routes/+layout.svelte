1ed129a7199dd2446ccac0d330e12fc5fdb49bdd
Ned Rihine <ned.rihine@gmail.com>
2025-04-19 19:06:55 +0900

    copyright を 2024 から 2025 に

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -21,7 +21,7 @@
 
   <footer class="mt-8 bg-white shadow dark:bg-gray-800">
     <div class="container mx-auto px-4 py-6 text-center">
-      <p>&copy; 2024 うぃーくりー・あんだーわーるど・にゅーす. All rights reserved.</p>
+      <p>&copy; 2025 うぃーくりー・あんだーわーるど・にゅーす. All rights reserved.</p>
     </div>
   </footer>
 </div>
