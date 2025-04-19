48b324d2a2036f68680a92e761d48a36a1d6afe2
Ned Rihine <ned.rihine@gmail.com>
2025-04-19 22:25:11 +0900

    時刻表示を ISO8601 に

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -19,7 +19,7 @@
           </a>
         </h2>
         <div class="mb-4 text-gray-600 dark:text-gray-400">
-          {new Date(post.date).toLocaleDateString()}
+          {new Date(post.date).toLocaleString('ja-JP', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', hour12: false }).replace(/\//g, '-').replace(',', 'T')}
         </div>
         <p class="text-gray-700 dark:text-gray-300">
           {post.description}
--- src/routes/blog/posts/[slug]/+page.svelte
+++ src/routes/blog/posts/[slug]/+page.svelte
@@ -11,7 +11,7 @@
   <header class="mb-8">
     <h1 class="mb-2 text-4xl font-bold">{data.post.title}</h1>
     <div class="text-gray-600 dark:text-gray-400">
-      {new Date(data.post.date).toLocaleDateString()}
+      {new Date(data.post.date).toLocaleString('ja-JP', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', hour12: false }).replace(/\//g, '-').replace(',', 'T')}
     </div>
   </header>
 
