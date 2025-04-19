180f9bace9324f2b17668ca886f0ef8e97cf2afa
Ned Rihine <ned.rihine@gmail.com>
2025-04-19 15:47:03 +0900

    ChatGPT によれば `@sveltejs/adapter-cloudflare` から `@sveltejs/adapter-static` に変更にした方がいいとのこと
    - `@sveltejs/adapter-cloudflare` を使うには function モードにする必要があるとのこと

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,16 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
-        adapter: adapter()
+        adapter: adapter({
+            pages: 'build',
+            assets: 'build',
+            fallback: 'index.html',
+            precompress: false
+        })
     },
     extensions: ['.svelte', '.md'],
     preprocess: [
